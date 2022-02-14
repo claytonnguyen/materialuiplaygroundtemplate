2c5dab99377ccfded067e75602bd2a43870e32d9
clayton <claytonnguyen3@gmail.com>
2022-02-13 20:13:28 -0800

    appbar and some grid containers added

---
+++ src/Body.js
@@ -0,0 +1,123 @@
+import React from 'react'
+import { styled } from '@mui/material/styles';
+
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+
+
+
+export default function Body(){
+    
+    const StyledPaper = styled(Paper)(({ theme }) => ({
+        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        ...theme.typography.body2,
+        padding: theme.spacing(8),
+        maxWidth: 400,
+        color: theme.palette.text.primary,
+      }));
+
+
+  return (
+        <Box sx={{ flexGrow: 1, overflow: 'hidden', px: 3 }}>
+            <StyledPaper
+                sx={{
+                my: 1,
+                mx: 'auto',
+                p: 2,
+                }}
+            >
+                <Grid container rowspacing={4} spacing={1}></Grid>
+                <Grid item xs={12} >
+                    <Grid item>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu metus eget tellus luctus gravida. Nulla in est sed purus congue rutrum non eget metus. Maecenas rhoncus dolor sed fermentum sollicitudin. Quisque vestibulum imperdiet justo ut sagittis. Curabitur gravida maximus nibh et sodales. Nam laoreet purus nec purus sollicitudin, nec volutpat leo ornare. Fusce tellus nunc, efficitur vitae suscipit quis, aliquet quis eros. Aenean id eleifend diam. Praesent consequat id metus condimentum iaculis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis pellentesque enim vitae tellus placerat cursus.</Grid>
+                </Grid>
+            </StyledPaper>
+            <StyledPaper
+                sx={{
+                my: 1,
+                mx: 'auto',
+                p: 2,
+                }}
+            >
+                <Grid container rowspacing={4} spacing={1}></Grid>
+                <Grid item xs={12}>
+                    <Grid item>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu metus eget tellus luctus gravida. Nulla in est sed purus congue rutrum non eget metus. Maecenas rhoncus dolor sed fermentum sollicitudin. Quisque vestibulum imperdiet justo ut sagittis. Curabitur gravida maximus nibh et sodales. Nam laoreet purus nec purus sollicitudin, nec volutpat leo ornare. Fusce tellus nunc, efficitur vitae suscipit quis, aliquet quis eros. Aenean id eleifend diam. Praesent consequat id metus condimentum iaculis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis pellentesque enim vitae tellus placerat cursus.</Grid>
+                </Grid>
+            </StyledPaper>
+            <StyledPaper
+                sx={{
+                my: 1,
+                mx: 'auto',
+                p: 2,
+                }}
+            >
+                <Grid container rowspacing={4} spacing={1}></Grid>
+                <Grid item xs={12}>
+                    <Grid item>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu metus eget tellus luctus gravida. Nulla in est sed purus congue rutrum non eget metus. Maecenas rhoncus dolor sed fermentum sollicitudin. Quisque vestibulum imperdiet justo ut sagittis. Curabitur gravida maximus nibh et sodales. Nam laoreet purus nec purus sollicitudin, nec volutpat leo ornare. Fusce tellus nunc, efficitur vitae suscipit quis, aliquet quis eros. Aenean id eleifend diam. Praesent consequat id metus condimentum iaculis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis pellentesque enim vitae tellus placerat cursus.</Grid>
+                </Grid>
+            </StyledPaper>
+            <StyledPaper
+                sx={{
+                my: 1,
+                mx: 'auto',
+                p: 2,
+                }}
+            >
+                <Grid container rowspacing={4} spacing={1}></Grid>
+                <Grid item xs={12}>
+                    <Grid item>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu metus eget tellus luctus gravida. Nulla in est sed purus congue rutrum non eget metus. Maecenas rhoncus dolor sed fermentum sollicitudin. Quisque vestibulum imperdiet justo ut sagittis. Curabitur gravida maximus nibh et sodales. Nam laoreet purus nec purus sollicitudin, nec volutpat leo ornare. Fusce tellus nunc, efficitur vitae suscipit quis, aliquet quis eros. Aenean id eleifend diam. Praesent consequat id metus condimentum iaculis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis pellentesque enim vitae tellus placerat cursus.</Grid>
+                </Grid>
+            </StyledPaper>
+            <StyledPaper
+                sx={{
+                my: 1,
+                mx: 'auto',
+                p: 2,
+                }}
+            >
+                <Grid container rowspacing={4} spacing={1}></Grid>
+                <Grid item xs={12}>
+                    <Grid item>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu metus eget tellus luctus gravida. Nulla in est sed purus congue rutrum non eget metus. Maecenas rhoncus dolor sed fermentum sollicitudin. Quisque vestibulum imperdiet justo ut sagittis. Curabitur gravida maximus nibh et sodales. Nam laoreet purus nec purus sollicitudin, nec volutpat leo ornare. Fusce tellus nunc, efficitur vitae suscipit quis, aliquet quis eros. Aenean id eleifend diam. Praesent consequat id metus condimentum iaculis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis pellentesque enim vitae tellus placerat cursus.</Grid>
+                </Grid>
+            </StyledPaper>
+            <StyledPaper
+                sx={{
+                my: 1,
+                mx: 'auto',
+                p: 2,
+                }}
+            >
+                <Grid container rowspacing={4} spacing={2}></Grid>
+                <Grid item xs={12}>
+                    <Grid item>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu metus eget tellus luctus gravida. Nulla in est sed purus congue rutrum non eget metus. Maecenas rhoncus dolor sed fermentum sollicitudin. Quisque vestibulum imperdiet justo ut sagittis. Curabitur gravida maximus nibh et sodales. Nam laoreet purus nec purus sollicitudin, nec volutpat leo ornare. Fusce tellus nunc, efficitur vitae suscipit quis, aliquet quis eros. Aenean id eleifend diam. Praesent consequat id metus condimentum iaculis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis pellentesque enim vitae tellus placerat cursus.</Grid>
+                </Grid>
+            </StyledPaper>
+            <StyledPaper
+                sx={{
+                my: 1,
+                mx: 'auto',
+                p: 2,
+                }}
+            >
+                <Grid container spacing={1}></Grid>
+                <Grid item xs={12}>
+                    <Grid item>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu metus eget tellus luctus gravida. Nulla in est sed purus congue rutrum non eget metus. Maecenas rhoncus dolor sed fermentum sollicitudin. Quisque vestibulum imperdiet justo ut sagittis. Curabitur gravida maximus nibh et sodales. Nam laoreet purus nec purus sollicitudin, nec volutpat leo ornare. Fusce tellus nunc, efficitur vitae suscipit quis, aliquet quis eros. Aenean id eleifend diam. Praesent consequat id metus condimentum iaculis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis pellentesque enim vitae tellus placerat cursus.</Grid>
+                </Grid>
+            </StyledPaper>
+            <StyledPaper
+                sx={{
+                my: 1,
+                mx: 'auto',
+                p: 2,
+                }}
+            >
+                <Grid container rowspacing={4} spacing={1}></Grid>
+                <Grid item xs={12}>
+                    <Grid item>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu metus eget tellus luctus gravida. Nulla in est sed purus congue rutrum non eget metus. Maecenas rhoncus dolor sed fermentum sollicitudin. Quisque vestibulum imperdiet justo ut sagittis. Curabitur gravida maximus nibh et sodales. Nam laoreet purus nec purus sollicitudin, nec volutpat leo ornare. Fusce tellus nunc, efficitur vitae suscipit quis, aliquet quis eros. Aenean id eleifend diam. Praesent consequat id metus condimentum iaculis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis pellentesque enim vitae tellus placerat cursus.</Grid>
+                </Grid>
+            </StyledPaper>
+        </Box>
+  );
+}
